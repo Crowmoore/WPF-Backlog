62e1fcb8c395258264b22b8506d33f18937f05a1
Joel Kortelainen <Joel Kortelainen>
2016-03-01 16:41:27 +0200

    Verify login credentials from database

---
+++ Backlog/Login.cs
@@ -0,0 +1,103 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Backlog
+{
+    class Login
+    {
+        private string dbPass;
+        private string connectionString;
+        private string username;
+        private string password;
+
+        public Login(string username, string password)
+        {
+            this.username = username;
+            this.password = MD5ForPHP(password);
+            this.dbPass = GetDatabaseInfo();
+            this.connectionString = "user=H3090;database=H3090_1;server=mysql.labranet.jamk.fi;password=" + this.dbPass + ";";
+        }
+
+        private string MD5ForPHP(string textToHash)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] unhashed = Encoding.UTF8.GetBytes((textToHash).ToLower());
+            byte[] hashed = md5.ComputeHash(unhashed);
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var b in hashed)
+            {
+                builder.Append(b.ToString("x").ToLower());
+            }
+            return builder.ToString();
+        }
+
+        private int GetRowCount(MySqlCommand command)
+        {
+            int count = 0;
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                count++;
+            }
+            return count;
+        }
+
+        public bool CheckCredentials()
+        {
+            MySqlConnection connection = new MySqlConnection(this.connectionString);
+            try
+            {
+                connection.Open();
+                string commandText = "SELECT * FROM user WHERE uid = @UID AND password = @PASSWORD";
+                MySqlCommand command = new MySqlCommand(commandText, connection);
+                command.Prepare();
+                command.Parameters.AddWithValue("@UID", this.username);
+                command.Parameters.AddWithValue("@PASSWORD", this.password);
+                command.ExecuteNonQuery();
+                int rowCount = GetRowCount(command);
+                if (rowCount == 1)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("connection: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private string GetDatabaseInfo()
+        {
+            string dbPassword = "";
+            try
+            {
+                using (StreamReader reader = new StreamReader("db.txt"))
+                {
+                    dbPassword = reader.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dbPassword;
+        }
+    }
+}
--- Backlog/MainWindow.xaml.cs
+++ Backlog/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace Backlog
 {
@@ -24,5 +27,30 @@ namespace Backlog
         {
             InitializeComponent();
         }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            txtEmail.Visibility = Visibility.Visible;
+            tbEmail.Visibility = Visibility.Visible;
+            btnLogin.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+            Login login = new Login(txtUsername.Text, txtPassword.Text);
+            if(login.CheckCredentials())
+            {
+                MessageBox.Show("Successfull login");
+            }
+            else
+            {
+                MessageBox.Show("Login unsuccessfull");
+            }
+            
+        }
+
+        
+
     }
 }
